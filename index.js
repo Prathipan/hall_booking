9aa32ef1071636f84b5f0943d9354f3d03158d64
Prathipan <prathipan24p@gmail.com>
2022-06-26 11:37:21 +0530

    dotenv commit

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get("/customers" , (req,res) => {
 res.send(filteredcustomers)
 })
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server started in ${PORT}`);
 });
